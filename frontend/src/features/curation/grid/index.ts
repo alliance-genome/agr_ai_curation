29b03ed702cd657534ea4adc8593dd7641217603
Chris Tabone <christabone@gmail.com>
2026-08-11 17:30:26 +0000

    feat(curation): add horizontal grid projection model

---
+++ frontend/src/features/curation/grid/index.ts
@@ -0,0 +1,14 @@
+export {
+  buildHorizontalGridModel,
+  HORIZONTAL_GRID_CONTEXT_COLUMN_KEY,
+} from './horizontalGridModel'
+export type {
+  BuildHorizontalGridModelInput,
+  HorizontalGridColumn,
+  HorizontalGridContextCell,
+  HorizontalGridFieldCell,
+  HorizontalGridModel,
+  HorizontalGridRow,
+  HorizontalGridRowContext,
+  HorizontalGridValidationProjection,
+} from './horizontalGridModel'
